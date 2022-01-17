5c7712de9d7ec337380a2f0fefa364e4c5998849
MinZombie <sg.min0410@gmail.com>
2022-01-17 16:10:17 +0900

    Project setup

---
+++ SimpleMemo/Controller/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMemo
+//
+//  Created by 민선기 on 2022/01/17.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
